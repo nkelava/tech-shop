413c22488461eab1c0172d8b59a981d6d616110b
Nikola Kelava <nikola.kelava94@gmail.com>
2022-07-06 21:42:48 +0200

    Add wishlist empty controller

---
+++ TechStoreBackend/src/TechStore.API/Controllers/WishlistController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TechStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+        public readonly IMapper _mapper;
+
+        public WishlistController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
