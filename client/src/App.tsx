eb46f63f453f5d6fd443ffd99a446b879f6adc09
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-20 22:58:59 +0100

    Add route to init not found page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
+import NotFound from "./pages/NotFound";
 
 export function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </div>
   );
