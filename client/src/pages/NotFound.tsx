4019c4bba4cd6326102c8bac2317bd056c6ed1db
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-20 23:30:49 +0100

    Change not found page text

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
 function NotFound() {
     return(
         <>
-            <h1>NotFound</h1>
+            <h1>OH N<sup>4</sup>O<sup>4</sup>! You have found the page that does not exist.</h1>
+            <p>
+                Let's get you back home.
+            </p>
+
         </>
     )
 }
