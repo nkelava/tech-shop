a6ad7a8ac64cebcb462245b4fff9bbf79dadc233
Nikola Kelava <nikola.kelava94@gmail.com>
2023-04-25 23:59:20 +0200

    Add navbar

--- TechStoreFrontend/src/App.vue
+++ TechStoreFrontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import TheNavbar from '@/components/TheNavbar.vue'
 </script>
 
 <template>
   <div>
+    <TheNavbar />
     <RouterView />
   </div>
 </template>
