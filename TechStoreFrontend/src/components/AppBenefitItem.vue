0d6675714cd038d37977f057364c9f31a83b8fed
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-12 20:04:26 +0200

    Separate benefit section into components

---
+++ TechStoreFrontend/src/components/AppBenefitItem.vue
@@ -0,0 +1,65 @@
+<script setup>
+const { icon, title, text } = defineProps(["icon", "title", "text"]);
+</script>
+
+<template>
+  <div class="benefit-item">
+    <img :src="icon" alt="home icon" class="benefit-item__img" />
+    <div class="benefit-item__content">
+      <h2>{{ title }}</h2>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.benefit-item {
+  background-color: var(--ts-c-bg-light);
+  border-right: 1px solid var(--ts-c-bg-dark);
+  color: var(--ts-c-text-dark);
+  padding: 1rem 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.benefit-item > * {
+  flex-wrap: wrap;
+}
+
+.benefit-item:first-child {
+  border-radius: 10px 0 0 10px;
+}
+
+.benefit-item:last-child {
+  border-radius: 0 10px 10px 0;
+}
+
+.benefit-item__img {
+  width: 48px;
+  height: 48px;
+  margin-right: 1rem;
+}
+
+.benefit-item__content h2 {
+  font-size: 1rem;
+}
+
+.benefit-item__content p {
+  color: var(--ts-c-primary-soft);
+}
+
+@media only screen and (max-width: 1200px) {
+  .benefit-item,
+  .benefit-item:first-child,
+  .benefit-item:last-child {
+    border-radius: 10px;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .benefit-item {
+    border-radius: 10px;
+  }
+}
+</style>
---
+++ TechStoreFrontend/src/components/TheBenefitList.vue
@@ -0,0 +1,62 @@
+<script setup>
+import BenefitItem from "@/components/AppBenefitItem.vue";
+
+import HomeIcon from "../assets/icons/house.png";
+import RefundIcon from "../assets/icons/refund.png";
+import SupportIcon from "../assets/icons/shield.png";
+import RocketIcon from "../assets/icons/rocket.png";
+</script>
+
+<template>
+  <div class="benefits-container">
+    <BenefitItem :icon="HomeIcon" title="Home Shipping" text="Lorem, ipsum." />
+    <BenefitItem :icon="RefundIcon" title="100% Refund" text="Lorem, ipsum." />
+    <BenefitItem :icon="SupportIcon" title="Client Support" text="Lorem, ipsum." />
+    <BenefitItem :icon="RocketIcon" title="Fast Delivery" text="Lorem, ipsum." />
+  </div>
+</template>
+
+<style scoped>
+.benefits-container {
+  margin-top: 2rem;
+  display: grid;
+  grid-template-areas: "home refund support delivery";
+  justify-content: center;
+}
+
+.benefits-container:nth-child(1) {
+  grid-area: home;
+}
+
+.benefits-container:nth-child(2) {
+  grid-area: refund;
+}
+
+.benefits-container:nth-child(3) {
+  grid-area: support;
+}
+
+.benefits-container:nth-child(4) {
+  grid-area: delivery;
+}
+
+@media only screen and (max-width: 1200px) {
+  .benefits-container {
+    gap: 0.5rem;
+    grid-template-areas:
+      "home refund"
+      "support delivery";
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .benefits-container {
+    gap: 0.5rem;
+    grid-template-areas:
+      "home"
+      "refund"
+      "support"
+      "delivery";
+  }
+}
+</style>
