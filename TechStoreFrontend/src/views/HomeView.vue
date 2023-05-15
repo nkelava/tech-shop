c7ad30db6b45bca00ed588f6de904898edfd47c3
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-15 21:49:24 +0200

    Remove ad banner section code from home view

--- TechStoreFrontend/src/views/HomeView.vue
+++ TechStoreFrontend/src/views/HomeView.vue
@@ -2,11 +2,8 @@
 import AppCard from "@/components/AppCard.vue";
 import TheSlider from "@/components/TheSlider.vue";
 import TheBenefitList from "@/components/TheBenefitList.vue";
+import TheBanners from "@/components/TheBanners.vue";
 import TheNewsletters from "@/components/TheNewsletters.vue";
-
-import DellAdImage from "../assets/images/test/banners/dell.jpg";
-import LogitechAdImage from "../assets/images/test/banners/logitech.jpg";
-import AsusAdImage from "../assets/images/test/banners/asus.png";
 </script>
 
 <template>
@@ -21,19 +18,7 @@ import AsusAdImage from "../assets/images/test/banners/asus.png";
     </div>
     <!-- ------------ -->
     <!-- Promotions section -->
-    <div class="promotions">
-      <div class="left">
-        <img :src="DellAdImage" alt="" />
-      </div>
-      <div class="right">
-        <div class="top">
-          <img :src="AsusAdImage" alt="" />
-        </div>
-        <div class="bottom">
-          <img :src="LogitechAdImage" alt="" />
-        </div>
-      </div>
-    </div>
+    <TheBanners />
     <!-- ----------- -->
     <!-- Top rated, New arrivals, Hot offers section - TODO: product list could be a separate component -->
     <div class="topnewhot">
@@ -63,47 +48,6 @@ import AsusAdImage from "../assets/images/test/banners/asus.png";
 
 .best-sellers h3 {
   color: var(--ts-c-text-highlight);
-}
-
-.promotions {
-  background-color: var(--ts-c-bg-light);
-  height: 500px;
-  width: 100%;
-  display: flex;
-  margin: 2rem 0;
-  padding: 2rem;
-}
-
-.left,
-.top,
-.bottom {
-  background-color: var(--ts-c-secondary-darker);
-}
-
-.left,
-.right {
-  width: 50%;
-  margin: 1rem;
-}
-
-.right {
-  display: block;
-}
-
-.top {
-  margin-bottom: 40px;
-}
-
-.top,
-.bottom {
-  height: 45%;
-}
-
-.left img,
-.top img,
-.bottom img {
-  height: 100%;
-  width: 100%;
 }
 
 .links a:first-child {
