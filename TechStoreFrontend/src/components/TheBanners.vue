8de65ab7269298d3942a0523926975ff17f08575
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-15 21:48:26 +0200

    Add ad banners section component

---
+++ TechStoreFrontend/src/components/TheBanners.vue
@@ -0,0 +1,58 @@
+<script setup>
+import DellAdImage from "@/assets/images/test/banners/dell.jpg";
+import LogitechAdImage from "@/assets/images/test/banners/logitech.jpg";
+import AsusAdImage from "@/assets/images/test/banners/asus.png";
+</script>
+
+<template>
+  <div class="banners">
+    <img :src="DellAdImage" alt="" />
+    <img :src="LogitechAdImage" alt="" />
+    <img :src="LogitechAdImage" alt="" />
+    <img :src="AsusAdImage" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.banners {
+  background-color: var(--ts-c-bg-light);
+  height: 40em;
+  padding: 2rem;
+  display: grid;
+  grid-template-areas:
+    "first second"
+    "third fourth";
+  gap: 1rem;
+}
+
+.banners:nth-child(1) {
+  grid-area: first;
+}
+
+.banners:nth-child(2) {
+  grid-area: second;
+}
+
+.banners:nth-child(3) {
+  grid-area: third;
+}
+
+.banners:nth-child(4) {
+  grid-area: fourth;
+}
+
+.banners img {
+  width: 100%;
+  height: 100%;
+}
+
+@media only screen and (max-width: 500px) {
+  .banners {
+    grid-template-areas:
+      "first"
+      "second"
+      "third"
+      "fourth";
+  }
+}
+</style>
