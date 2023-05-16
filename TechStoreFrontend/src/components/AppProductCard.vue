c5b80ae4b87e9e6f1b3c5bdb7150a98fee9d8689
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-16 15:21:38 +0200

    Add separate components for product list and card

---
+++ TechStoreFrontend/src/components/AppProductCard.vue
@@ -0,0 +1,57 @@
+<script setup>
+import pcImage from "@/assets/images/test/products/pc.png";
+
+const { product } = defineProps(["product"]);
+</script>
+
+<template>
+  <!-- TODO: add favorites and cart icon on hover -->
+  <div class="card">
+    <img :src="pcImage" alt="" class="card__image" />
+    <h2 class="card__title">{{ product.name }}</h2>
+    <p class="card__summary">{{ product.summary }}</p>
+    <!-- TODO: add currency as separate property -->
+    <h3 class="card__price">{{ product.price }}</h3>
+    <button>VIEW MORE</button>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: var(--ts-c-bg-light);
+  border-radius: 10px;
+  color: var(--ts-c-text-dark);
+  padding: 1rem;
+  text-align: center;
+  width: 200px;
+}
+
+.card__image {
+  width: 150px;
+  height: 150px;
+}
+
+.card__title {
+  font-size: 1rem;
+  margin: 1rem;
+}
+
+.card__summary {
+  font-size: 12px;
+  color: var(--ts-c-secondary-darker);
+}
+
+.card__price {
+  margin: 1rem 0;
+}
+
+.card button {
+  background-color: var(--ts-c-bg-dark);
+  border: none;
+  border-radius: 30px;
+  color: var(--ts-c-text-light);
+  font-weight: bold;
+  padding: 0.5rem;
+  width: 10rem;
+}
+</style>
---
+++ TechStoreFrontend/src/components/AppProductList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AppProductCard from "@/components/AppProductCard.vue";
+
+const { products } = defineProps(["products"]);
+</script>
+
+<template>
+  <div class="products">
+    <AppProductCard v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<style scoped>
+.products {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 250px);
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
