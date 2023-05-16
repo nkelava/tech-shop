6dc4c9d4425f7e90ef799c3a349b734a374dc5a5
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-16 22:09:46 +0200

    Refactor newsletter section

--- TechStoreFrontend/src/components/TheNewsletters.vue
+++ TechStoreFrontend/src/components/TheNewsletters.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="newsletters">
-    <div class="left">
-      <div>
-        <h1>Subscribe to newsletters</h1>
-        <h3>Lorem ipsum dolor sit.</h3>
-      </div>
+    <div class="newsletters__heading">
+      <h1>Subscribe to newsletter</h1>
+      <h3>Lorem ipsum dolor sit.</h3>
     </div>
-    <div class="right">
+    <div class="newsletters__subscribe">
       <input type="text" placeholder="Enter yout email..." />
-      <button>Subscribe</button>
+      <input type="submit" value="Subscribe" />
     </div>
   </div>
 </template>
@@ -16,21 +14,76 @@
 <style scoped>
 .newsletters {
   background-color: var(--ts-c-bg-highlight);
-  height: 200px;
-  margin-bottom: 3rem;
-  display: flex;
+  padding: 2rem;
+  display: grid;
+  gap: 5rem;
+  grid-template-areas: "heading input";
 }
 
-.left {
+.newsletters:nth-child(1) {
+  grid-area: heading;
+}
+
+.newsletters:nth-child(2) {
+  grid-area: input;
+}
+
+.newsletters__heading {
   color: var(--ts-c-text-dark);
+  text-align: end;
 }
 
-.left,
-.right {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
+.newsletters__heading h3 {
+  color: var(--ts-c-primary-soft);
+}
+
+.newsletters__subscribe {
+  position: relative;
+  align-self: center;
+  justify-self: start;
+}
+
+input[type="text"] {
+  border: none;
+  border-radius: 2rem;
+  outline: none;
+  color: var(--ts-c-text-dark);
+  font-weight: bold;
+  padding-left: 1rem;
+  height: 2rem;
+  width: 40em;
+}
+
+input[type="submit"] {
+  background-color: var(--ts-c-primary-soft);
+  border: none;
+  border-radius: 2rem;
+  outline: none;
+  color: white;
+  font-weight: bold;
+  height: 2rem;
+  width: 7rem;
+  position: absolute;
+  right: 0;
+}
+
+@media only screen and (max-width: 1000px) {
+  .newsletters {
+    grid-template-areas:
+      "heading"
+      "input";
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .newsletters__heading {
+    text-align: center;
+  }
+
+  .newsletters__subscribe,
+  .newsletters__subscribe input {
+    width: 100%;
+  }
 }
 </style>
