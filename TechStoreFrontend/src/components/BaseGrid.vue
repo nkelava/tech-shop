ae14cbd767fdaaaa38f8931e096bf78dfe8bf8f7
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-18 23:39:54 +0200

    Add base grid component

---
+++ TechStoreFrontend/src/components/BaseGrid.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.grid {
+  padding: 2rem 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 250px);
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
