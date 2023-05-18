5b7ae64b07a3d63c2d068d85fb112805cd8ffae4
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-18 23:39:40 +0200

    Add newsletters to main layout

--- TechStoreFrontend/src/App.vue
+++ TechStoreFrontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from "vue-router";
 import TheHeader from "@/components/TheHeader.vue";
+import TheNewsletters from "@/components/TheNewsletters.vue";
 import TheFooter from "@/components/TheFooter.vue";
 </script>
 
@@ -8,6 +9,7 @@ import TheFooter from "@/components/TheFooter.vue";
   <div>
     <TheHeader />
     <RouterView />
+    <TheNewsletters />
     <TheFooter />
   </div>
 </template>
