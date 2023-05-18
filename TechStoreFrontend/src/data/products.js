258c5090e23943c566152b7af36eef38060ec1bd
Nikola Kelava <nikola.kelava94@gmail.com>
2023-05-18 23:40:35 +0200

    Add images to test product data

--- TechStoreFrontend/src/data/products.js
+++ TechStoreFrontend/src/data/products.js
@@ -1,65 +1,68 @@
+import pcImage from "@/assets/images/test/products/pc.png";
+import headphonesImage from "@/assets/images/test/products/headphones.webp";
+
 export const products = [
   {
     id: 1,
     name: "Test 1",
     price: "123,41$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "pc.png",
+    img: pcImage,
   },
   {
     id: 2,
     name: "Test 2",
     price: "123,42$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "headphones.webp",
+    img: headphonesImage,
   },
   {
     id: 3,
     name: "Test 3",
     price: "123,43$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "pc.png",
+    img: pcImage,
   },
   {
     id: 4,
     name: "Test 4",
     price: "123,44$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "headphones.webp",
+    img: headphonesImage,
   },
   {
     id: 5,
     name: "Test 5",
     price: "123,45$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "pc.png",
+    img: pcImage,
   },
   {
     id: 6,
     name: "Test 6",
     price: "123,46$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "headphones.webp",
+    img: headphonesImage,
   },
   {
     id: 7,
     name: "Test 7",
     price: "123,47$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "pc.png",
+    img: pcImage,
   },
   {
     id: 8,
     name: "Test 8",
     price: "123,48$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "headphones.webp",
+    img: headphonesImage,
   },
   {
     id: 9,
     name: "Test 9",
     price: "123,49$",
     summary: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    image: "pc.png",
+    img: pcImage,
   },
 ];
