795f78d0275493983617c8695d2ae4cfed5be3d7
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-02 23:42:40 +0200

    add specification mock data

---
+++ TechStoreFrontend/src/database/seed/specifications.js
@@ -0,0 +1,82 @@
+export const specificationsDb = [
+  {
+    productId: 1,
+    attributeId: 10,
+    valueId: 14,
+  },
+  {
+    productId: 1,
+    attributeId: 1,
+    valueId: 4,
+  },
+  {
+    productId: 1,
+    attributeId: 2,
+    valueId: 9,
+  },
+  {
+    productId: 1,
+    attributeId: 3,
+    valueId: 11,
+  },
+  {
+    productId: 1,
+    attributeId: 4,
+    valueId: 19,
+  },
+  {
+    productId: 1,
+    attributeId: 6,
+    valueId: 7,
+  },
+  {
+    productId: 1,
+    attributeId: 7,
+    valueId: 22,
+  },
+  {
+    productId: 1,
+    attributeId: 9,
+    valueId: 27,
+  },
+  {
+    productId: 3,
+    attributeId: 10,
+    valueId: 12,
+  },
+  {
+    productId: 3,
+    attributeId: 1,
+    valueId: 4,
+  },
+  {
+    productId: 3,
+    attributeId: 2,
+    valueId: 12,
+  },
+  {
+    productId: 3,
+    attributeId: 3,
+    valueId: 10,
+  },
+  {
+    productId: 3,
+    attributeId: 4,
+    valueId: 19,
+  },
+  {
+    productId: 3,
+    attributeId: 6,
+    valueId: 6,
+  },
+  {
+    productId: 3,
+    attributeId: 7,
+    valueId: 20,
+  },
+  {
+    productId: 3,
+    attributeId: 9,
+    valueId: 27,
+  },
+];
