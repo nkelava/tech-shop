626088ba1ac44b6e3d9b5df9ef7685bd3609a7ea
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-09 13:03:11 +0200

    Setup vuetify

--- TechStoreFrontend/src/main.js
+++ TechStoreFrontend/src/main.js
@@ -4,10 +4,30 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// Custom Styles
 import "./assets/main.css";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
