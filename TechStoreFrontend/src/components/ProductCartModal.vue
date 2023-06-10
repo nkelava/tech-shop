4dc9eee28d01572c5d080194059f8921c3059b3c
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-10 19:27:55 +0200

    Add product cart modal

---
+++ TechStoreFrontend/src/components/ProductCartModal.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import CartIcon from "@/assets/icons/header/cart.png";
+import ProductCartItem from "./ProductCartItem.vue";
+
+const dialog = ref(false);
+const counter = 3;
+const title = "Your Shopping Cart";
+
+const toggleDialog = () => {
+  dialog.value = !dialog.value;
+};
+</script>
+
+<template>
+  <div>
+    <v-btn color="primary" @click="toggleDialog">
+      <img :src="CartIcon" alt="favorites icon" class="dropdown__icon" />
+      <sub> ({{ counter }}) </sub>
+    </v-btn>
+    <v-dialog v-model="dialog" persistent width="auto" class="dialog">
+      <ProductCartItem @toggleDialog="toggleDialog" :title="title" />
+    </v-dialog>
+  </div>
+</template>
+
+<style scoped></style>
