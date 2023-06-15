459743d9ae892372f5565e82c55c0cc7ad34b518
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-15 20:45:42 +0200

    Add user order history tab

---
+++ TechStoreFrontend/src/components/OrderDetails.vue
@@ -0,0 +1,73 @@
+<script setup>
+const props = defineProps(["order"]);
+
+const category = "laptops";
+const subcategory = "notebooks";
+</script>
+
+<template>
+  <v-container class="main pa-10 rounded-lg">
+    <v-row class="order-info mb-3">
+      <v-col>
+        <v-row> Date Placed </v-row>
+        <v-row> {{ props.order.datePlaced }} </v-row>
+      </v-col>
+      <v-col>
+        <v-row> Order number </v-row>
+        <v-row> {{ props.order.orderNumber }} </v-row>
+      </v-col>
+      <v-col>
+        <v-row> Total Amount </v-row>
+        <v-row> {{ props.order.totalAmount }}$</v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-table class="w-100 transparent">
+        <thead>
+          <tr>
+            <th class="text-left">Product</th>
+            <th class="text-left">Price</th>
+            <th class="text-left">Status</th>
+            <th class="text-left">Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in props.order.products" :key="product.id">
+            <td class="d-flex align-center py-10">
+              <img :src="product.img" class="mr-4" height="64" width="64" alt="" />
+              {{ product.name }}
+            </td>
+            <td>{{ product.price }}$</td>
+            <td>{{ order.status }}</td>
+            <td>
+              <router-link
+                :to="{
+                  name: 'product',
+                  params: { category: category, subcategory: subcategory, productId: product.id },
+                }"
+                >View Product</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.main {
+  background-color: var(--ts-c-primary-soft);
+}
+
+.order-info {
+  background: var(--ts-c-bg-dark);
+  border-radius: 10px;
+  padding: 10px 20px;
+}
+
+.transparent {
+  background: transparent;
+  color: var(--ts-c-secondary);
+}
+</style>
---
+++ TechStoreFrontend/src/components/UserOrders.vue
@@ -0,0 +1,33 @@
+<script setup>
+import OrderDetails from "@/components/OrderDetails.vue";
+import { getProducts } from "@/database/services/productService";
+const orders = [
+  {
+    id: 1,
+    datePlaced: new Date().toLocaleString(),
+    orderNumber: "12321321",
+    totalAmount: 123,
+    status: "Delivered",
+    products: getProducts().slice(0, 6),
+  },
+  {
+    id: 2,
+    datePlaced: new Date().toLocaleString(),
+    orderNumber: "123215434",
+    totalAmount: 234,
+    status: "Delivery In Progress",
+    products: getProducts().slice(7, 11),
+  },
+];
+</script>
+
+<template>
+  <v-container class="main pa-10 rounded-lg">
+    <v-row>
+      <h2>My Orders</h2>
+    </v-row>
+    <v-row v-for="order in orders" :key="order.id">
+      <OrderDetails :order="order" />
+    </v-row>
+  </v-container>
+</template>
