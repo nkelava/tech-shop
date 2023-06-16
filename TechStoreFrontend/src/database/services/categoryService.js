e57471248fad8d60239f10b824cf18672240dcfc
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-16 22:01:57 +0200

    Add get all categories service method

--- TechStoreFrontend/src/database/services/categoryService.js
+++ TechStoreFrontend/src/database/services/categoryService.js
@@ -1,5 +1,9 @@
 import { categoriesDb } from "../seed/categories.js";
 
+export function getCategories() {
+  return categoriesDb;
+}
+
 export function getCategoryBySlug(categorySlug) {
   return categoriesDb.find((category) => category.slug === categorySlug);
 }
