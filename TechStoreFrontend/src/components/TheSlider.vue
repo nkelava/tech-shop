80826d4b955155a03ce922c19b08ca252ad79cfa
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-17 19:52:34 +0200

    Add hero slider

--- TechStoreFrontend/src/components/TheSlider.vue
+++ TechStoreFrontend/src/components/TheSlider.vue
@@ -1,16 +1,22 @@
 <script setup>
+import Skola from "@/assets/images/test/carousel/skola.webp";
+import SpiderMan from "@/assets/images/test/carousel/spider-man.webp";
+import Visa from "@/assets/images/test/carousel/visa.webp";
+
+// TODO: either separate hero slider from this or add images as props
+const images = [Skola, SpiderMan, Visa];
 const { height } = defineProps(["height"]);
 </script>
 
 <template>
-  <div class="slider">
-    <img src="" alt="" />
-  </div>
+  <v-carousel class="carousel" hide-delimiters show-arrows="hover">
+    <v-carousel-item v-for="(image, i) in images" :key="i" :src="image" cover></v-carousel-item>
+  </v-carousel>
 </template>
 
 <style scoped>
-.slider {
-  background: var(--ts-c-bg-light);
-  height: v-bind(height);
+.carousel {
+  height: v-bind(height) !important;
+  width: 100%;
 }
 </style>
