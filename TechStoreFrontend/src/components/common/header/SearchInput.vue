e6e183a031a3ce4393dfcbe58e704219f3017076
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-17 18:35:21 +0200

    Add search input component

---
+++ TechStoreFrontend/src/components/common/header/SearchInput.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+import SearchIcon from "@/assets/icons/header/search.png";
+
+const search = ref("");
+const emit = defineEmits(["search"]);
+
+function handleSearch() {
+  emit("search", search.value);
+}
+</script>
+
+<template>
+  <div class="search-container">
+    <v-text-field
+      v-model="search"
+      placeholder="Search..."
+      variant="solo"
+      density="compact"
+      hide-details
+      rounded
+    />
+    <v-btn class="search__btn" @click="handleSearch" rounded>
+      <img :src="SearchIcon" alt="" />
+    </v-btn>
+  </div>
+</template>
+
+<style scoped>
+.search-container {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  min-width: 250px;
+  max-width: 800px;
+  position: relative;
+}
+
+.search__btn {
+  background-color: var(--ts-c-bg-light);
+  min-height: 100%;
+  position: absolute;
+  right: 0;
+}
+</style>
--- TechStoreFrontend/src/components/common/header/TheHeader.vue
+++ TechStoreFrontend/src/components/common/header/TheHeader.vue
@@ -1,31 +1,28 @@
 <script setup>
 import TheTopBar from "./TheTopBar.vue";
+import SearchInput from "./SearchInput.vue";
 import FavoritesModal from "@/components/common/header/favorites/FavoritesModal.vue";
 import CartModal from "@/components/common/header/cart/CartModal.vue";
 import UserProfileDropdown from "@/components/common/header/account/UserProfileDropdown.vue";
 import TheNavbar from "./TheNavbar.vue";
-import SearchIcon from "@/assets/icons/header/search.png";
 
 // TODO: implement user state
 const isSignedIn = true;
+
+function handleSearch(searchText) {
+  console.log("Search: ", searchText);
+}
 </script>
 
 <template>
-  <header class="header">
+  <header>
     <!-- CONTACT INFO AND LOGIN/REGISTER -->
     <the-top-bar />
     <!-- SHOP LOGO, SEARCH BAR AND ACTION ICONS -->
     <div class="header__main">
-      <div class="header__brand">
-        <h1>TechShop</h1>
-      </div>
-      <div class="header__search">
-        <input class="search__input" type="text" placeholder="Search..." />
-        <button class="search__btn">
-          <img :src="SearchIcon" alt="phone icon" class="search__icon" />
-        </button>
-      </div>
-      <div class="header__dropdowns">
+      <h1>TechShop</h1>
+      <search-input @search="handleSearch" />
+      <div class="d-flex">
         <favorites-modal />
         <cart-modal />
         <user-profile-dropdown v-if="isSignedIn" />
@@ -41,54 +38,8 @@ const isSignedIn = true;
   color: var(--ts-c-text-light);
   display: flex;
   flex-wrap: wrap;
-  align-items: center;
-}
-
-.header__main {
   justify-content: space-between;
-  padding: 0.5rem 2rem;
-}
-
-.dropdown__icon {
-  margin-inline: 0.5rem;
-}
-
-.header__search {
-  position: relative;
-  display: flex;
   align-items: center;
-  width: 500px;
-  height: 25px;
-}
-
-.search__input {
-  border-radius: 30px;
-  padding-left: 1rem;
-  width: 500px;
-  height: 100%;
-  border: none;
-}
-
-.search__btn {
-  background-color: var(--ts-c-bg-light);
-  border: none;
-  border-radius: 30px;
-  width: 3rem;
-  height: 25px;
-  position: absolute;
-  right: 0;
-}
-
-.search-btn img {
-  vertical-align: middle;
-}
-
-.header__dropdowns {
-  display: flex;
-}
-
-.dropdown__btn {
-  border: none;
-  background: transparent;
+  padding: 1rem 2rem;
 }
 </style>
