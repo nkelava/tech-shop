2969762b26cc7455df66a87270dab7e1b1f43545
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-22 10:54:58 +0200

    Refactor product slider

--- TechStoreFrontend/src/components/ProductSlider.vue
+++ TechStoreFrontend/src/components/ProductSlider.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 import ProductCard from "@/components/ProductCard.vue";
 
-const { products } = defineProps(["products"]);
+const props = defineProps(["header", "products"]);
 const breakpoints = {
   400: {
     itemsToShow: 1,
@@ -15,39 +15,53 @@ const breakpoints = {
     snapAlign: "center",
   },
   900: {
-    itemsToShow: 3.5,
+    itemsToShow: 2,
     snapAlign: "center",
   },
   // 1024 and up
   1024: {
-    itemsToShow: 4,
+    itemsToShow: 3,
     snapAlign: "start",
   },
   1400: {
-    itemsToShow: 5,
+    itemsToShow: 4,
     snapAlign: "start",
   },
   1800: {
-    itemsToShow: 6,
+    itemsToShow: 5,
     snapAlign: "start",
   },
 };
 </script>
 <template>
-  <carousel :items-to-show="5.5" :wrap-around="true" :breakpoints="breakpoints">
-    <slide v-for="product in products" :key="product">
-      <product-card :product="product" />
-    </slide>
+  <div class="carousel-container">
+    <h2>{{ props.header }}</h2>
+    <hr />
+    <carousel :items-to-show="4.5" :wrap-around="true" :breakpoints="breakpoints">
+      <slide v-for="product in props.products" :key="product">
+        <product-card :product="product" />
+      </slide>
 
-    <template #addons>
-      <navigation />
-      <pagination />
-    </template>
-  </carousel>
+      <template #addons>
+        <navigation />
+      </template>
+    </carousel>
+  </div>
 </template>
 
 <style scoped>
+.carousel-container {
+  max-width: 70%;
+  margin: 0 auto;
+}
+
 .carousel__pagination {
   margin-top: 1.5rem;
+}
+
+hr {
+  border: 1px solid var(--ts-c-primary-darker);
+  border-radius: 5px;
+  margin-bottom: 2rem;
 }
 </style>
