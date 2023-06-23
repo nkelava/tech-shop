11ad407a1b53b0263913c0d10c1e70f4fbde3b73
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-23 20:53:18 +0200

    Add products sliders as tabbed section

--- TechStoreFrontend/src/views/HomeView.vue
+++ TechStoreFrontend/src/views/HomeView.vue
@@ -11,6 +11,7 @@ const bestSellers = ref([]);
 const newArrivals = ref([]);
 const hotOffers = ref([]);
 const topRated = ref([]);
+const tab = ref("new");
 
 onMounted(() => {
   bestSellers.value = getProducts();
@@ -30,18 +31,55 @@ onMounted(() => {
       <product-grid :products="bestSellers" />
     </div>
     <the-banners />
-    <div class="carousel-container">
-      <h1>New Arrivals</h1>
-      <hr />
-      <product-slider :products="bestSellers" />
+    <div class="tabs-container">
+      <div class="tabs__header">
+        <v-tabs v-model="tab">
+          <v-tab value="new">
+            <h2 class="tab">New Arrivals</h2>
+          </v-tab>
+          <v-tab value="hot">
+            <h2 class="tab">Hot Offers</h2>
+          </v-tab>
+          <v-tab value="top">
+            <h2 class="tab">Top Rated</h2>
+          </v-tab>
+        </v-tabs>
+      </div>
+      <v-card-text>
+        <v-window v-model="tab" class="tabs__content">
+          <v-window-item value="new">
+            <product-slider :products="newArrivals" />
+          </v-window-item>
+
+          <v-window-item value="hot">
+            <product-slider :products="hotOffers" />
+          </v-window-item>
+
+          <v-window-item value="top">
+            <product-slider value="three" :products="topRated" />
+          </v-window-item>
+        </v-window>
+      </v-card-text>
     </div>
   </div>
 </template>
 
 <style scoped>
 .grid-container,
-.carousel-container {
+.tabs-container {
   max-width: 70%;
   margin: 6rem auto;
+}
+
+.tabs__header {
+  margin-bottom: 3rem;
+}
+
+.tab {
+  text-transform: capitalize;
+}
+
+.tabs__content {
+  overflow: visible;
 }
 </style>
