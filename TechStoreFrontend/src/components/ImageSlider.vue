cfa5c2bddaa3d522fb2b85fe45695ee60b14536b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-06-23 14:33:08 +0200

    Fix carousel losing style on refresh

--- TechStoreFrontend/src/components/ImageSlider.vue
+++ TechStoreFrontend/src/components/ImageSlider.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Carousel, Navigation, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 import Skola from "@/assets/images/test/carousel/skola.webp";
 import SpiderMan from "@/assets/images/test/carousel/spider-man.webp";
 import Visa from "@/assets/images/test/carousel/visa.webp";
@@ -7,7 +8,6 @@ import Hyperx from "@/assets/images/test/carousel/hyperx.jpg";
 
 // TODO: either separate hero slider from this or add images as props
 const images = [Skola, SpiderMan, Visa, Hyperx];
-
 const props = defineProps(["maxHeight"]);
 </script>
 
@@ -26,9 +26,13 @@ const props = defineProps(["maxHeight"]);
 </template>
 
 <style scoped>
+.carousel {
+  min-height: 200px;
+  max-height: v-bind("props.maxHeight");
+}
+
 .carousel__item {
   height: 100%;
-  max-height: v-bind("props.maxHeight");
   width: 100%;
 }
 
