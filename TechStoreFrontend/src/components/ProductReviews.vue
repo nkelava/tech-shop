54b9ede0885eaaa8b3b050defcf06f3b194c9bee
Nikola Kelava <n.kelava@dotien.hr>
2023-08-14 01:04:13 +0200

    Add new review submit

--- TechStoreFrontend/src/components/ProductReviews.vue
+++ TechStoreFrontend/src/components/ProductReviews.vue
@@ -1,41 +1,7 @@
 <script setup>
 import { computed, ref } from "vue";
+import { reviews } from "@/database/seed/reviews";
 
-const reviews = [
-  {
-    name: "Ali Connors",
-    rating: 4,
-    review: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-  },
-  {
-    name: "Alex Scott",
-    rating: 3.5,
-    review:
-      "Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend.",
-  },
-  {
-    name: "Sandra Adams",
-    rating: 5,
-    review: "Do you have Paris recommendations? Have you ever been?",
-  },
-
-  {
-    name: "Ali Connorsa",
-    rating: 4,
-    review: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-  },
-  {
-    name: "Alex Scott",
-    rating: 3.5,
-    review:
-      "Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend.",
-  },
-  {
-    name: "Sandra Adams",
-    rating: 5,
-    review: "Do you have Paris recommendations? Have you ever been?",
-  },
-];
 const pageState = ref({
   currentPage: 1,
   itemsPerPage: 5,
@@ -48,6 +14,15 @@ const currentPageItems = computed(() => {
     pageState.value.currentPage * pageState.value.itemsPerPage
   );
 });
+
+const rating = ref(1);
+const message = ref("");
+
+function handleReview() {
+  if (message.value.length > 0) {
+    console.log(`User: user; Rating: ${rating.value}; Message: ${message.value}`);
+  }
+}
 </script>
 
 <template>
@@ -57,7 +32,7 @@ const currentPageItems = computed(() => {
         <div class="review">
           <span class="review__name">{{ review.name }}</span>
           <div class="review__info">
-            <v-rating v-model="review.rating" size="small" density="compact" readonly> </v-rating>
+            <v-rating v-model="review.rating" size="small" density="compact" readonly />
             <p>{{ new Date().toJSON().slice(0, 10).replace(/-/g, "/") }}</p>
           </div>
           <span class="review__text">{{ review.review }}</span>
@@ -73,6 +48,24 @@ const currentPageItems = computed(() => {
           </v-col>
         </v-row>
       </v-container>
+      <div class="review-input ts-container">
+        <h2>Leave your review</h2>
+        <span class="review__rating">
+          Rating: <v-rating v-model="rating" density="compact" />
+        </span>
+        <v-textarea
+          v-model="message"
+          class="review__textarea"
+          label="Your review"
+          variant="outlined"
+          prepend-inner-icon="mdi-comment"
+          hide-details="true"
+          no-resize
+          clearable
+          @keydown.enter.prevent
+        ></v-textarea>
+        <v-btn class="review__btn" @click="handleReview">Submit</v-btn>
+      </div>
     </v-list>
   </v-card>
 </template>
@@ -107,5 +100,34 @@ const currentPageItems = computed(() => {
 
 .review__text {
   margin-top: 10px;
+}
+
+.review-input {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+}
+
+.review__rating {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.review__textarea {
+  width: 100%;
+  max-width: 400px;
+  font-weight: 14px;
+}
+
+::v-deep .v-label {
+  font-size: 12px;
+}
+
+.review__btn {
+  text-transform: capitalize;
+  background-color: var(--ts-c-bg-dark);
+  color: var(--ts-c-text-light);
 }
 </style>
---
+++ TechStoreFrontend/src/database/seed/reviews.js
@@ -0,0 +1,37 @@
+const reviews = [
+  {
+    name: "Ali Connors",
+    rating: 4,
+    review: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  },
+  {
+    name: "Alex Scott",
+    rating: 3.5,
+    review:
+      "Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend.",
+  },
+  {
+    name: "Sandra Adams",
+    rating: 5,
+    review: "Do you have Paris recommendations? Have you ever been?",
+  },
+
+  {
+    name: "Ali Connorsa",
+    rating: 4,
+    review: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+  },
+  {
+    name: "Alex Scott",
+    rating: 3.5,
+    review:
+      "Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend. Wish I could come, but I'm out of town this weekend.",
+  },
+  {
+    name: "Sandra Adams",
+    rating: 5,
+    review: "Do you have Paris recommendations? Have you ever been?",
+  },
+];
+
+export { reviews };
