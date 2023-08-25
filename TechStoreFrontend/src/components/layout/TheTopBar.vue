374f61c12f901dac3ed15e32ed398b4fd073afe5
Nikola Kelava <nikola.kelava94@gmail.com>
2023-08-25 20:59:51 +0200

    Update missed email content

--- TechStoreFrontend/src/components/layout/TheTopBar.vue
+++ TechStoreFrontend/src/components/layout/TheTopBar.vue
@@ -12,7 +12,7 @@ const userStore = useUserStore();
   <div class="header-top ts-container">
     <div class="header-top__contact">
       <contact-info :imgUrl="EmailIcon" imgAlt="email icon">
-        Email: tech-store@gmail.com
+        Email: info.techplanet@gmail.com
       </contact-info>
       <contact-info :imgUrl="PhoneIcon" imgAlt="phone icon"> Phone: 123-456-7890 </contact-info>
     </div>
