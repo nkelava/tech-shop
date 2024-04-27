09b833f7aac2308846c5643240c94183cf449b6b
Nikola Kelava <nikola.kelava94@gmail.com>
2024-04-27 23:15:44 +0200

    Refactor global axios, add axios private interceptor

--- TechStoreFrontend/src/api/axios.config.js
+++ TechStoreFrontend/src/api/axios.config.js
@@ -1,6 +1,3 @@
-const userLocalStorage = JSON.parse(localStorage.getItem("user"));
-const accessToken = userLocalStorage && userLocalStorage.user ? userLocalStorage.user.token : null;
-
 const defaultOptions = {
   baseURL: import.meta.env.VITE_API_BASE_URL,
 };
@@ -9,7 +6,6 @@ const privateOptions = {
   ...defaultOptions,
   headers: {
     "Content-type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
   },
   withCredentials: true,
   credentials: "include",
