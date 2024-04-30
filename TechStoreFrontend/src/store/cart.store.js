14e65dd894d2ef5f16d87b248a241653a26c8582
Nikola Kelava <nikola.kelava94@gmail.com>
2024-04-30 19:59:08 +0200

    Improve cart store

--- TechStoreFrontend/src/store/cart.store.js
+++ TechStoreFrontend/src/store/cart.store.js
@@ -1,4 +1,9 @@
 import { defineStore } from "pinia";
+import { useToast } from "vue-toastification";
+import { axiosPrivate } from "@/api/axios";
+import { useUserStore } from "@/store";
+
+const toast = useToast();
 
 export const useCartStore = defineStore("cart", {
   state: () => ({
@@ -28,51 +33,201 @@ export const useCartStore = defineStore("cart", {
         return state.items[itemIndex].quantity * state.items[itemIndex].price;
       }
     },
+    isUserLoggedIn: () => {
+      const user = useUserStore();
+      return user.isLoggedIn;
+    },
   },
   actions: {
-    addItem(item) {
-      const itemExists = this.items.find((i) => i.id === item.id);
+    async addItem(item, quantity = 1) {
+      const itemExists = this.isProductAlreadyInCart(item?.id);
 
-      if (itemExists) return;
+      if (itemExists >= 0) {
+        toast.info("This product is already in your cart.");
+        return;
+      }
 
-      item = { ...item, quantity: 1 };
+      try {
+        if (this.isUserLoggedIn) {
+          const resp = await axiosPrivate
+            .post("/carts", {
+              productId: item?.id,
+              quantity,
+            })
+            .catch((error) => {
+              toast.error(
+                "Oops! Something went wrong while adding the product to your cart. Please try again later or contact support for assistance."
+              );
+              console.log(error);
+            });
 
-      this.items.push(item);
-      this.persistData();
-    },
-    incrementQuantity(id) {
-      const itemIndex = this.items.findIndex((item) => item.id === id);
+          if (!resp?.data) return;
+        }
 
-      if (itemIndex >= 0) {
-        this.items[itemIndex].quantity += 1;
+        item = { ...item, quantity };
+        this.items.push(item);
         this.persistData();
+        toast.success("Success! The product has been added to your cart. Happy shopping!");
+      } catch {
+        toast.error(
+          "Oops! Something went wrong while adding the product to your cart. Please try again later or contact support for assistance."
+        );
       }
     },
-    decrementQuantity(id) {
-      const itemIndex = this.items.findIndex((item) => item.id === id);
+    async removeItem(productId) {
+      try {
+        if (this.isUserLoggedIn) {
+          const resp = await axiosPrivate.delete(`/carts/${productId}`).catch((error) => {
+            toast.error(
+              "Oops! Something went wrong while trying to remove the item from your cart. Please try again later or contact customer support for assistance."
+            );
+            console.log(error);
+          });
 
-      if (itemIndex >= 0) {
-        if (this.items[itemIndex].quantity > 1) {
+          if (!resp?.data) {
+            toast.error(
+              "Oops! Something went wrong while trying to remove the item from your cart. Please try again later or contact customer support for assistance."
+            );
+            return;
+          }
+        }
+
+        this.items = this.items.filter((item) => item.id != productId);
+        this.persistData();
+      } catch {
+        toast.error(
+          "Oops! Something went wrong while trying to remove the item from your cart. Please try again later or contact customer support for assistance."
+        );
+      }
+    },
+    async incrementQuantity(productId) {
+      const itemIndex = this.isProductAlreadyInCart(productId);
+
+      if (itemIndex < 0) return;
+
+      try {
+        if (this.isUserLoggedIn) {
+          const resp = await axiosPrivate
+            .post("/carts", {
+              productId,
+              quantity: this.items[itemIndex].quantity + 1,
+            })
+            .catch((error) => {
+              toast.error(
+                "Oops! Something went wrong while incrementing product quantity. Please try again later or contact support for assistance."
+              );
+              console.log(error);
+            });
+
+          if (!resp?.data) return;
+        }
+
+        this.items[itemIndex].quantity += 1;
+        this.persistData();
+      } catch {
+        toast.error(
+          "Oops! Something went wrong while incrementing product quantity. Please try again later or contact support for assistance."
+        );
+      }
+    },
+    async decrementQuantity(productId) {
+      const itemIndex = this.isProductAlreadyInCart(productId);
+
+      if (itemIndex < 0) return;
+
+      if (this.items[itemIndex].quantity > 1) {
+        try {
+          if (this.isUserLoggedIn) {
+            const resp = await axiosPrivate
+              .post("/carts", {
+                productId,
+                quantity: this.items[itemIndex].quantity - 1,
+              })
+              .catch((error) => {
+                toast.error(
+                  "Oops! Something went wrong while decrementing product quantity. Please try again later or contact support for assistance."
+                );
+                console.log(error);
+              });
+
+            if (!resp?.data) return;
+          }
+
           this.items[itemIndex].quantity -= 1;
           this.persistData();
+        } catch {
+          toast.error(
+            "Oops! Something went wrong while decrementing product quantity. Please try again later or contact support for assistance."
+          );
         }
       }
     },
-    removeItem(id) {
-      this.items = this.items.filter((item) => item.id != id);
-      this.persistData();
-    },
+    isProductAlreadyInCart(productId) {
+      if (this.items.length < 1) return -1;
 
+      return this.items.findIndex((item) => item.id === productId);
+    },
     persistData() {
       localStorage.setItem("cart", JSON.stringify(this.items));
     },
+    async loadData() {
+      // try {
+      //   const data = this.isUserLoggedIn
+      //     ? await axiosPrivate
+      //         .get("/carts")
+      //         .then((resp) => {
+      //           let formattedCart = [];
 
-    loadData() {
-      const data = localStorage.getItem("cart");
+      //           if (resp?.data?.products) {
+      //             formattedCart = resp?.data?.products.map((product) => {
+      //               return { ...product.product, quantity: product?.quantity };
+      //             });
 
-      if (data) {
-        this.items = JSON.parse(data);
+      //             return JSON.stringify(formattedCart);
+      //           }
+
+      //           return [];
+      //         })
+      //         .catch((error) => console.log(error))
+      //     : localStorage.getItem("cart");
+
+      //   if (data) {
+      //     this.items = JSON.parse(data);
+      //     this.persistData();
+      //   }
+      // } catch {
+      //   console.log("There was an error loading cart data...");
+      // }
+      try {
+        const data =
+          this.isUserLoggedIn &&
+          (await axiosPrivate
+            .get("/carts")
+            .then((resp) => {
+              let formattedCart = [];
+
+              if (resp?.data?.products) {
+                formattedCart = resp?.data?.products.map((product) => {
+                  return { ...product.product, quantity: product?.quantity };
+                });
+
+                return JSON.stringify(formattedCart);
+              }
+
+              return [];
+            })
+            .catch((error) => console.log(error)));
+
+        if (data) {
+          this.items = JSON.parse(data);
+          this.persistData();
+        }
+      } catch {
+        console.log("There was an error loading cart data...");
       }
+    },
+    async clearStore() {
+      this.$reset();
     },
   },
   persist: true,
