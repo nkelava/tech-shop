682dc6c349b56fbd2eac2d329a30ab0d72447763
Nikola Kelava <nikola.kelava94@gmail.com>
2024-05-11 20:21:01 +0200

    Add delete constant messages

---
+++ TechStoreFrontend/src/constants/messages/delete.js
@@ -0,0 +1,3 @@
+export const ITEM_DELETE_SUCCESS = "The item has been successfully deleted.";
+export const ITEM_DELETE_FAIL =
+  "Oops! Something went wrong while deleting the item. Please try again later.";
